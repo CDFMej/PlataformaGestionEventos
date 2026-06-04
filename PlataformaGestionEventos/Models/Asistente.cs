773e08400747c959bf71b7cca2d0bdc7c91fac7e
SanchezBessy <bessymata46@gmail.com>
2026-06-03 21:48:49 -0600

    creacion modelo asistente

---
+++ PlataformaGestionEventos/Models/Asistente.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoReservaciones.Models;
+
+public class Asistente
+{
+    [Key]
+    public int AsistenteId { get; set; }
+
+    [Required(ErrorMessage = "Ingrese el nombre")]
+    [StringLength(100)]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "Ingrese la identidad")]
+    [StringLength(20)]
+    public string Identidad { get; set; }
+
+    [Required(ErrorMessage = "Ingrese el correo")]
+    [EmailAddress]
+    public string Correo { get; set; }
+
+    [Required(ErrorMessage = "Ingrese el teléfono")]
+    [Phone]
+    public string Telefono { get; set; }
+
+    // Navegación
+    public ICollection<Inscripcion>? Inscripciones { get; set; }
+}

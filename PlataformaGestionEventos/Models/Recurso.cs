ceec1785a83c1f8ef91a31701421ffb16f8418ca
steven21029 <stevenrivera21029@gmail.com>
2026-06-03 22:24:51 -0600

    creacion de modelo

---
+++ PlataformaGestionEventos/Models/Recurso.cs
@@ -0,0 +1,20 @@
+namespace ProyectoReservaciones.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+
+public class Recurso
+{
+    [Key]
+    public int RecursoId { get; set; }
+    [Required(ErrorMessage = "Ingrese el nombre del recurso")]
+    [StringLength(100)]
+    public string Nombre { get; set; }
+    [Required(ErrorMessage = "Ingrese el tipo de recurso")]
+    [StringLength(100)]
+    public string Tipo { get; set; }
+    public bool Disponible { get; set; } = true;
+
+    public int cantidad { get; set; }
+    public ICollection<RecursoEvento>? RecursosEvento { get; set; }
+}

9532a0e57218934c510dbbb5d4408482617accf5
Riccy96 <138516625+Riccy96@users.noreply.github.com>
2026-06-04 15:32:03 -0600

    Modelo de Inscripción

---
+++ PlataformaGestionEventos/Models/Inscripcion.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoReservaciones.Models;
+
+public class Inscripcion
+{
+    [Key]
+    public int InscripcionId { get; set; }
+
+    public DateTime FechaInscripcion { get; set; } = DateTime.Now;
+
+    [Required]
+    public string Estado { get; set; } = "Confirmado";
+
+ 
+    public int EventoId { get; set; }
+
+    [ForeignKey("EventoId")]
+    public Evento? Evento { get; set; }
+
+
+    public int AsistenteId { get; set; }
+
+    [ForeignKey("AsistenteId")]
+    public Asistente? Asistente { get; set; }
+}
